0cb561a580ca1efe2cdc0266d408872624467b9a
Guohui Xiao <xiao@inf.unibz.it>
2019-10-16 23:34:51 +0200

    ontop script insider docker and cors setting

--- docker/endpoint.sh
+++ docker/endpoint.sh
@@ -36,8 +36,8 @@ psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME \
     $COPY_POSTGRES_DB < /opt/ontop/src/create_views.sql
 
 echo "Entrypoint - Starting Ontop Endpoint"
-java -cp ./lib/*:./jdbc/* -Dlogback.configurationFile=./log/logback.xml \
-    it.unibz.inf.ontop.cli.Ontop endpoint \
+/opt/ontop/ontop.sh endpoint \
     --ontology=$ONTOLOGY_FILE \
     --mapping=$MAPPING_FILE \
-    --properties=$PROPERTIES_FILE
+    --properties=$PROPERTIES_FILE \
+    --cors-allowed-origins='*'
