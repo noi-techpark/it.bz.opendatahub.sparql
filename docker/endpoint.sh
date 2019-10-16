673b76d9c2f22daa08fde42034e905dfb72dd1c5
Alex Lanz <alex@krumer.it>
2019-10-16 11:10:30 +0200

    Deployment

--- docker/endpoint.sh
+++ docker/endpoint.sh
@@ -3,6 +3,7 @@
 set -e
 set -o pipefail
 
+echo "Entrypoint - Dumping old DB"
 export PGPASSWORD="$ORIGINAL_POSTGRES_PASSWORD"
 pg_dump --host=$ORIGINAL_POSTGRES_HOST --username=$ORIGINAL_POSTGRES_USERNAME $ORIGINAL_POSTGRES_DB \
     --exclude-table="AspNetRoles" \
@@ -14,6 +15,7 @@ pg_dump --host=$ORIGINAL_POSTGRES_HOST --username=$ORIGINAL_POSTGRES_USERNAME $O
     --exclude-table="tripplaners" \
     > dump.sql
 
+echo "Entrypoint - Restoring new DB"
 export PGPASSWORD="$COPY_POSTGRES_PASSWORD"
 psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRES_DB \
     --command="DROP SCHEMA IF EXISTS public;"
@@ -24,9 +26,11 @@ psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRE
 
 rm -rf dump.sql
 
+echo "Entrypoint - Create views"
 psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRES_DB \
     < /opt/ontop/src/create_views.sql
 
+echo "Entrypoint - Starting Ontop Endpoint"
 java -cp ./lib/*:./jdbc/* -Dlogback.configurationFile=./log/logback.xml \
     it.unibz.inf.ontop.cli.Ontop endpoint \
     --ontology=$ONTOLOGY_FILE \
