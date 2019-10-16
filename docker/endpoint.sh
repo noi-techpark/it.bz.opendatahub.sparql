9b4330400ae9bc45f66a980f17a028d8d9dbbcad
Alex Lanz <alex@krumer.it>
2019-10-16 11:17:38 +0200

    Deployment

--- docker/endpoint.sh
+++ docker/endpoint.sh
@@ -5,30 +5,30 @@ set -o pipefail
 
 echo "Entrypoint - Dumping old DB"
 export PGPASSWORD="$ORIGINAL_POSTGRES_PASSWORD"
-pg_dump --host=$ORIGINAL_POSTGRES_HOST --username=$ORIGINAL_POSTGRES_USERNAME $ORIGINAL_POSTGRES_DB \
-    --exclude-table="AspNetRoles" \
-    --exclude-table="AspNetUserClaims" \
-    --exclude-table="AspNetUserLogins" \
-    --exclude-table="AspNetUserRoles" \
-    --exclude-table="AspNetUsers" \
-    --exclude-table="users" \
-    --exclude-table="tripplaners" \
-    > dump.sql
+pg_dump --host=$ORIGINAL_POSTGRES_HOST --username=$ORIGINAL_POSTGRES_USERNAME \
+    --exclude-table-data="AspNetRoles" \
+    --exclude-table-data="AspNetUserClaims" \
+    --exclude-table-data="AspNetUserLogins" \
+    --exclude-table-data="AspNetUserRoles" \
+    --exclude-table-data="AspNetUsers" \
+    --exclude-table-data="users" \
+    --exclude-table-data="tripplaners" \
+    $ORIGINAL_POSTGRES_DB > dump.sql
 
 echo "Entrypoint - Restoring new DB"
 export PGPASSWORD="$COPY_POSTGRES_PASSWORD"
-psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRES_DB \
-    --command="DROP SCHEMA IF EXISTS public;"
-psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRES_DB \
-    --command="CREATE SCHEMA public;"
-psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRES_DB \
-    < dump.sql
+psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME \
+    --command="DROP SCHEMA IF EXISTS public;" $COPY_POSTGRES_DB
+psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME \
+    --command="CREATE SCHEMA public;" $COPY_POSTGRES_DB
+psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME \
+    $COPY_POSTGRES_DB < dump.sql
 
 rm -rf dump.sql
 
 echo "Entrypoint - Create views"
-psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME $COPY_POSTGRES_DB \
-    < /opt/ontop/src/create_views.sql
+psql --host=$COPY_POSTGRES_HOST --username=$COPY_POSTGRES_USERNAME \
+    $COPY_POSTGRES_DB < /opt/ontop/src/create_views.sql
 
 echo "Entrypoint - Starting Ontop Endpoint"
 java -cp ./lib/*:./jdbc/* -Dlogback.configurationFile=./log/logback.xml \
