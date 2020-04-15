63fa579443a11335eb968e63b688db062ce3ba06
Alex Lanz <alex@krumer.it>
2020-04-15 11:10:49 +0200

    Adjusting migrations

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 echo "Entrypoint - Run Flyway Migrations"
-/usr/local/bin/flyway migrate
+/usr/local/bin/flyway -locations=filesystem:/opt/ontop/sql migrate
 
 echo "Entrypoint - Starting Ontop Endpoint"
 /opt/ontop/entrypoint.sh
