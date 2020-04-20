eb4ba304473ff114ec72ddf466be04c3531b6950
Peter Moser <p.moser@noi.bz.it>
2020-04-20 15:09:57 +0200

    Flyway: Drop subscription if exist

--- sql/V2__replication.sql
+++ sql/V2__replication.sql
@@ -1,6 +1,6 @@
 ALTER SUBSCRIPTION vkgsubscription DISABLE;
 ALTER SUBSCRIPTION vkgsubscription SET (slot_name = NONE);
-DROP SUBSCRIPTION vkgsubscription;
+DROP SUBSCRIPTION IF EXISTS vkgsubscription;
 
 CREATE SUBSCRIPTION vkgsubscription 
     CONNECTION 'host=${original_host} dbname=${original_db} user=${original_user} password=${original_password}' 
