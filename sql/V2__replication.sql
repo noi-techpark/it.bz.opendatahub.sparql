70675166b521bbde5fdecc908af3b70efbcfe6cb
Peter Moser <p.moser@noi.bz.it>
2020-04-20 15:05:27 +0200

    Flyway: Drop subscription before to be sure

--- sql/V2__replication.sql
+++ sql/V2__replication.sql
@@ -1,3 +1,7 @@
+ALTER SUBSCRIPTION vkgsubscription DISABLE;
+ALTER SUBSCRIPTION vkgsubscription SET (slot_name = NONE);
+DROP SUBSCRIPTION vkgsubscription;
+
 CREATE SUBSCRIPTION vkgsubscription 
     CONNECTION 'host=${original_host} dbname=${original_db} user=${original_user} password=${original_password}' 
     PUBLICATION vkgpublication
