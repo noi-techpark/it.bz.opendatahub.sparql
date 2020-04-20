838ae10132c382541f3d1c6c98ef9bbccce894ed
Peter Moser <p.moser@noi.bz.it>
2020-04-20 14:54:26 +0200

    Flyway: Do not create slots, if they already exist

--- sql/V2__replication.sql
+++ sql/V2__replication.sql
@@ -1,3 +1,4 @@
 CREATE SUBSCRIPTION vkgsubscription 
     CONNECTION 'host=${original_host} dbname=${original_db} user=${original_user} password=${original_password}' 
-    PUBLICATION vkgpublication;
+    PUBLICATION vkgpublication
+    WITH (create_slot = false);
