5ec44973ad7701672cf9017cbb17204b71c3f400
Peter Moser <p.moser@noi.bz.it>
2020-04-20 20:54:32 +0200

    Flyway: Do not alter non-existing subscriptions

--- sql/V2.2__replication.sql
+++ sql/V2.2__replication.sql
@@ -3,4 +3,4 @@ DROP SUBSCRIPTION IF EXISTS ${subscription_name};
 CREATE SUBSCRIPTION ${subscription_name}
     CONNECTION 'host=${original_host} dbname=${original_db} user=${original_user} password=${original_password}' 
     PUBLICATION vkgpublication
-    WITH (create_slot = false);
+    WITH (create_slot = true);
