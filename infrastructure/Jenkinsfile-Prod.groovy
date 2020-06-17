896ed88c1e0e4aef67cabb3a00d883aec750b87a
Alex Lanz <alex@krumer.it>
2020-06-17 14:06:11 +0200

    Fixing deployment

--- infrastructure/Jenkinsfile-Prod.groovy
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -79,7 +79,7 @@ pipeline {
                sshagent(['jenkins-ssh-key']) {
                     sh """
                         (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
-                        (cd infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER}")
+                        (cd infrastructure/ansible && ansible-playbook --limit=prod deploy.yml --extra-vars "release_name=${BUILD_NUMBER}")
                     """
                 }
             }
