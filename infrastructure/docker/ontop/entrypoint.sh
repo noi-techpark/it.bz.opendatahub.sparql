6829f6027fc58774ab97709fb65036506012f085
Peter Moser <p.moser@noi.bz.it>
2020-12-02 18:24:56 +0100

    infra: do not exit on unbound variable error

--- infrastructure/docker/ontop/entrypoint.sh
+++ infrastructure/docker/ontop/entrypoint.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 echo "Entrypoint - Run Flyway Migrations"
-/usr/local/bin/flyway -locations=filesystem:/opt/ontop/sql "${EXTRA_FLYWAY_OPTIONS}" migrate
+/usr/local/bin/flyway -locations=filesystem:/opt/ontop/sql "${EXTRA_FLYWAY_OPTIONS-}" migrate
 
 echo "Entrypoint - Starting Ontop Endpoint"
 /opt/ontop/entrypoint.sh
