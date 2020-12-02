ff55da0f15c7f73fe4bac26f68a8540504cde186
Peter Moser <p.moser@noi.bz.it>
2020-12-02 18:16:49 +0100

    infra: do not check for unbound variables in entrypoint script

--- infrastructure/docker/ontop/entrypoint.sh
+++ infrastructure/docker/ontop/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 echo "Entrypoint - Run Flyway Migrations"
 /usr/local/bin/flyway -locations=filesystem:/opt/ontop/sql "${EXTRA_FLYWAY_OPTIONS}" migrate
