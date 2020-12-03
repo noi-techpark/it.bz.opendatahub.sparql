b5c7b0e4bad2df17e5b1a62892c97754ed8d4179
Peter Moser <p.moser@noi.bz.it>
2020-12-03 17:13:19 +0100

    flyway: fix readonly user permissions

---
+++ sql/R__read_only_permissions.sql
@@ -0,0 +1,3 @@
+GRANT USAGE ON SCHEMA public TO ${copy_user_readonly};
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${copy_user_readonly};
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO ${copy_user_readonly};
