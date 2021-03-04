2ea8dac3eb8f6ef9aac89baf3ddd4ec899d3ced4
Peter Moser <p.moser@noi.bz.it>
2021-03-04 09:40:15 +0100

    sql: Do not create users (on tourism replica)
    Since the user that runs these scripts usually has not
    the privileges to create roles

--- sql/public/R__read_only_permissions.sql
+++ sql/public/R__read_only_permissions.sql
@@ -1,11 +1,21 @@
-DO
-$$
-    BEGIN
-        CREATE ROLE ${vkg_user_readonly};
-    EXCEPTION WHEN DUPLICATE_OBJECT THEN
-        RAISE NOTICE 'role ${vkg_user_readonly} exists, skipping...';
-    END
-$$;
+--
+-- Roles should exist already. The role that runs this command has no privileges
+-- to create roles (on production environments), so we will get a permission
+-- denied even if the role exists already. To prevent that we need to create a
+-- corresponding user beforehand (manually).
+--
+-- DO
+-- $$
+--     BEGIN
+--         CREATE ROLE ${vkg_user_readonly};
+--     EXCEPTION WHEN DUPLICATE_OBJECT THEN
+--         RAISE NOTICE 'role ${vkg_user_readonly} exists, skipping...';
+--     END
+-- $$;
+--
+-- Simple alternative:
+-- CREATE ROLE ${vkg_user_readonly};
+--
 
 GRANT USAGE ON SCHEMA ${tourism_schema_vkg} TO ${vkg_user_readonly};
 GRANT SELECT ON ALL TABLES IN SCHEMA ${tourism_schema_vkg} TO ${vkg_user_readonly};
