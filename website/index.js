0d5b72328611cae441b2837ad9c4fd8b864f6680
Ronald Domi <ronalddomi4@gmail.com>
2021-08-26 10:52:51 +0200

    website update with new queries and text

--- website/index.js
+++ website/index.js
@@ -3,131 +3,92 @@ var action;
 
 
       // specify id of element and optional scroll speed as arguments
-      var scrollToElement = function(el, ms){
-	  clearTimeout(action);
-        var speed = (ms) ? ms : 600;
-        $('html,body').animate({
-            scrollTop: $(el).offset().top
-        }, speed);
-		
-		
-    }
-	
-	// 
-	
-	var animateButton = function(){
-
-	action = setTimeout(function() {
-
-	
-    $(".yasqe_queryButton").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  }, 600);
-  
-   
-      
-    }
-	
-	// open and select tab specifing id of element and optional scroll speed as arguments
-	var openTab = function(el){
-	
-		oldTab= yasgui.tabs[el];
-		//check if there is already a tab open for this query
-		if (!oldTab) {
-		yasgui.addTab(el);
-		} 
-		
-		return yasgui.selectTab(el);
-	}
-	
-
-
-
-      function playground() {
-		
-		tab = openTab("pl");
-		tab.rename("Playground");
-		
-        playgroundQuery = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-		SELECT * WHERE {
-		  ?sub ?pred ?obj .
-		} 
-		LIMIT 10
-        `
-		
-        tab.setQuery(playgroundQuery);
-    
-        tab.yasr.options.output = "table";
-		$("button.yasr_btn.select_table").click();
-		
-		scrollToElement('#yasgui', 600);
-		if (tab.yasr.somethingDrawn()) {
-		// query to override the results 
-		tab.query();
-		} else {
-		tab.yasr.warn("Press the play button to test the query.")
-		animateButton();
-		}
-		
-        
-        
-		
-		
-      }
-
-
-
-      function lodging() {
-
-		tab = openTab("ld");
-		tab.rename("Lodging");
-        lodgingBusinesses = `PREFIX schema: <http://schema.org/>
-PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-PREFIX : <http://noi.example.org/ontology/odh#>
-
-SELECT ?h ?pos ?posLabel ?posColor
-WHERE {
-  ?h a schema:LodgingBusiness ;
-     geo:defaultGeometry/geo:asWKT ?pos ;
-     schema:name ?posLabel .
-  #?h schema:containedInPlace/schema:name "Bozen"@de . # Uncomment for restricting to a municipality
-  FILTER (lang(?posLabel) = 'de')
-
-  # Colors
-  OPTIONAL {
-    ?h a schema:Campground .
-    BIND("chlorophyll,0.5" AS ?posColor) # Green
-  }
-    OPTIONAL {
-    ?h a schema:BedAndBreakfast .
-    BIND("viridis,0.1" AS ?posColor) # Purple
-  }
-  OPTIONAL {
-    ?h a schema:Hotel .
-    BIND("jet,0.3" AS ?posColor) # Light blue
-  }
-  OPTIONAL {
-    ?h a schema:Hostel .
-    BIND("jet,0.8" AS ?posColor) # Red
-  }
+var scrollToElement = function(el, ms){
+  	clearTimeout(action);
+	var speed = (ms) ? ms : 600;
+	$('html,body').animate({
+		scrollTop: $(el).offset().top
+	}, speed);
 }
-LIMIT 500
-        `
-        tab.setQuery(lodgingBusinesses);
 
-		
-		tab.yasr.options.output = "leaflet";
-		$("button.yasr_btn.select_leaflet").click();
-		
-		scrollToElement('#yasgui', 600);
-		if (tab.yasr.somethingDrawn()) {
-		// query to override the results 
-		tab.query()
-		} else {
-		tab.yasr.warn("Press the play button to test the query.");
-		animateButton();
-		}
-		
-        
-      }
+	//
+
+var animateButton = function(){
+	action = setTimeout(function() {
+		$(".yasqe_queryButton").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	}, 600);
+}
+
+	// open and select tab specifing id of element and optional scroll speed as arguments
+var openTab = function(el){
+
+	oldTab= yasgui.tabs[el];
+	//check if there is already a tab open for this query
+	if (!oldTab) {
+	yasgui.addTab(el);
+	}
+
+	return yasgui.selectTab(el);
+}
+
+
+function changeTab(tabNumber) {
+	var queryTab01 = document.getElementById('queryTab01');
+	var queryTab02 = document.getElementById('queryTab02');
+	var queryTab03 = document.getElementById('queryTab03');
+	var tab01 = document.getElementById('tab01');
+	var tab02 = document.getElementById('tab02');
+	var tab03 = document.getElementById('tab03');
+
+	queryTab01.classList.remove('is-active');
+	queryTab02.classList.remove('is-active');
+	queryTab03.classList.remove('is-active');
+	tab01.classList.remove('is-active');
+	tab02.classList.remove('is-active');
+	tab03.classList.remove('is-active');
+	switch (tabNumber) {
+		case '01' :
+			queryTab01.classList.add('is-active');
+			tab01.classList.add('is-active');
+			return;
+		case '02' :
+			queryTab02.classList.add('is-active');
+			tab02.classList.add('is-active');
+			return;
+		case '03' :
+			queryTab03.classList.add('is-active');
+			tab03.classList.add('is-active');
+			return;
+
+
+
+	}
+	// scrollToElement('#queries-tabs', 600)
+}
+
+function runQuery(menu, buttonNumber, output) {
+	tab = yasgui.selectTab("");
+	switch (menu) {
+		case 'Tourism':
+			currentQuery = queryList.Tourism[buttonNumber];
+			break;
+		case 'Environment':
+			currentQuery = queryList.Environment[buttonNumber];
+			break;
+		case 'Mobility':
+			currentQuery = queryList.Mobility[buttonNumber];
+			break;
+	}
+	tab.setQuery(currentQuery);
+	// tab.query();
+	switch (output) {
+		case 'table':
+			tab.yasr.options.output = "table";
+			break;
+		case 'leaflet':
+			tab.yasr.options.output = "leaflet";
+			break;
+
+	}
+	scrollToElement('#yasgui', 600);
+}
