a04222c3cb32787271a1483d4ba8f36c66e2fbf0
Fabio Endrizzi <fabio.endrizzibz@gmail.com>
2022-06-21 12:43:21 +0200

    mobility-sync: move to infrastructure/docker

--- infrastructure/docker/mobility-sync/release.sh
+++ infrastructure/docker/mobility-sync/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 MODULE=github.com/noi-techpark/it.bz.opendatahub.sparql/infrastructure/utils/mobility-sync
-IMAGE=odh-vkg-sync
+IMAGE=mobility-sync
 GIT_DESCRIPTION=$(git describe --always --dirty --tags --long)
 
 docker build -t $IMAGE:"$GIT_DESCRIPTION" -t $IMAGE:latest --build-arg module="$MODULE" --build-arg version="$GIT_DESCRIPTION" .
