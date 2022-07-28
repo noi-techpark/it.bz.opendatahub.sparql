6cc2bd1703a2e8d584a19f4f914f34415ed7d3a4
Fabio Endrizzi <fabio.endrizzibz@gmail.com>
2022-07-28 10:54:53 +0200

    mobility-sync: wait for ontop to finish bootstrapping

--- infrastructure/docker/mobility-sync/release.sh
+++ infrastructure/docker/mobility-sync/release.sh
@@ -3,7 +3,7 @@ MODULE=github.com/noi-techpark/it.bz.opendatahub.sparql/infrastructure/docker/mo
 IMAGE=mobility-sync
 GIT_DESCRIPTION=$(git describe --always --dirty --tags --long)
 
-docker build -t $IMAGE:"$GIT_DESCRIPTION" -t $IMAGE:latest --build-arg module="$MODULE" --build-arg version="$GIT_DESCRIPTION" .
+docker build -t $IMAGE:"$GIT_DESCRIPTION" -t $IMAGE:latest --build-arg module="$MODULE" --build-arg version="$GIT_DESCRIPTION" --target standalone .
 
 # for tag in $GIT_DESCRIPTION latest; do
 #   docker push $IMAGE:"$tag"
